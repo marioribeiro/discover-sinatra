86a2baef80cb81ba4acb876f9ee90f8fedfcb37b
“rogrenke” <roland@rolandgrenke.com>
2017-05-22 15:51:04 +0100

    Sinatra Views, creates new 'views' folder with index.erb file

--- app.rb
+++ app.rb
@@ -2,24 +2,6 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/' do
- "hello!"
-end
-
-get '/secret' do
- "This is our secret.. shhh"
-end
-
-get '/mario' do
-  "Mario"
-end
-
-get '/roland' do
-  "Roland"
-end
-
 get '/kitty' do
-  "<div style='border: 3px red dashed'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
