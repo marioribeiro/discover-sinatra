d09b8441d4480f07f4f9aad4e52b863cecde6280
Mário Ribeiro <mail@marioribeiro.com>
2017-05-23 10:01:42 +0100

    Add folder for assest (CSS/Img).
    - Add style.css

--- app.rb
+++ app.rb
@@ -2,11 +2,6 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/random-kitty' do
-  @name = ["Amigo","Oscar","Viking"].sample
-  erb(:index)
-end
-
 post '/named-kitty' do
   p params
   @name = params[:name]
